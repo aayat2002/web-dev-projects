17b50d36c980ff95a28be436e55bc4404042ab19
Sadqua parween <f.naaz.2002a@gmail.com>
2024-09-15 19:37:50 +0530

    personal-portfolio

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -12,18 +12,24 @@ export const Footer = () => {
         <Row className="align-items-center">
           <MailchimpForm />
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+            {/* <img src={logo} alt="Logo" /> */}
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/sadqua-parween/">
+                <img src={navIcon1} alt="Icon" />
+              </a>
+              <a href="https://github.com/aayat2002">
+                <img src={navIcon2} alt="Icon" />
+              </a>
+              {/* <a href="https://vercel.com/aayat2002s-projects">
+                <img src={navIcon3} alt="Icon" />
+              </a> */}
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            {/* <p>Copyright 2022. All Rights Reserved</p> */}
           </Col>
         </Row>
       </Container>
     </footer>
-  )
-}
+  );
+};
